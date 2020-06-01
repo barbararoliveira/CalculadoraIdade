5945f2908bed2c3801d8b5bb40b5b2ec9b3a95c4
Bárbara <babyr.oliveira@gmail.com>
2020-06-01 17:56:22 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculadoraIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DateTime barbara = new DateTime (2004, 04, 06, 16, 13, 00);
+            string barbara2 = barbara.ToShortDateString();
+            DateTime dtagora = DateTime.Now;
+
+            TimeSpan tempo =  dtagora - barbara;
+            int dias = tempo.Days;
+            
+            Console.ForegroundColor = ConsoleColor.Magenta; 
+            Console.WriteLine($"Eu nasci em {barbara2} e já vivi {dias} dias.");
+
+        }
+    }
+}
